402cd1e3248fa1099fc98832065b299b43ad2252
Tilen_G <43862401+TilenG2@users.noreply.github.com>
2023-02-16 09:09:49 +0100

    Create DN01.java

---
+++ Domace_Naloge/DN01.java
@@ -0,0 +1,12 @@
+public class DN01 {
+    public static void main(String[] args) {
+        System.out.println("Moje ime je: Tilen Gabršček");
+        System.out.println("Moja vpisna številka: 63220076\n");
+        System.out.println("*******************************************************");
+        System.out.println("*|*  Izjavljam,  da sem prebral/a vsebino  strani   *|*");
+        System.out.println("*|*  'Samostojna izdelava programov' na eUčilnici.  *|*");
+        System.out.println("*|*  Vse obveznosti predmeta Programiranje 2 bom    *|*");
+        System.out.println("*|*  skladno s temi navodili opravil/a samostojno.  *|*");
+        System.out.println("*******************************************************");
+    }
+}
