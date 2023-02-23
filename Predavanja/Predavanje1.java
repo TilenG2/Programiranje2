b55caaa93cbc86cdd90bbb42cc5cc4c7e648b4bc
Tilen_G <43862401+TilenG2@users.noreply.github.com>
2023-02-23 08:49:02 +0100

    tedn2

---
+++ Predavanja/Predavanje1.java
@@ -0,0 +1,9 @@
+package Predavanja;
+
+public class Predavanje1 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
