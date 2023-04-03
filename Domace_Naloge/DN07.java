0b9e1ec7205c0f9d1cff2d48cb5e92c876dec0ed
Tilen_G <43862401+TilenG2@users.noreply.github.com>
2023-04-03 16:20:20 +0200

    DN07

---
+++ Domace_Naloge/DN07.java
@@ -0,0 +1,65 @@
+public class DN07 {
+    public static void main(String[] args) throws Exception {
+        int naloga = Integer.parseInt(args[0]);
+        switch (naloga) {
+            case 1:
+                naloga1();
+                break;
+            case 2:
+                naloga2();
+                break;
+            case 3:
+                naloga3();
+                break;
+            case 4:
+                naloga4();
+                break;
+            case 5:
+                naloga5();
+                break;
+            case 6:
+                naloga6();
+                break;
+            case 7:
+                naloga7();
+                break;
+            case 8:
+                naloga8();
+                break;
+            case 9:
+                naloga9();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public static void naloga1() {
+    }
+
+    public static void naloga2() {
+    }
+
+    public static void naloga3() {
+    }
+
+    public static void naloga4() {
+    }
+
+    public static void naloga5() {
+    }
+
+    public static void naloga6() {
+    }
+
+    public static void naloga7() {
+    }
+
+    public static void naloga8() {
+    }
+
+    public static void naloga9() {
+    }
+
+}
