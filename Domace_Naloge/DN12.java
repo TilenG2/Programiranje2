2fc47344a2c73ce774ab88228af2899c4e50720f
Tilen_G <43862401+TilenG2@users.noreply.github.com>
2023-05-30 15:15:11 +0200

    DN12

---
+++ Domace_Naloge/DN12.java
@@ -0,0 +1,52 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class DN12 {
+
+    public static void main(String[] args) {
+        JFrame okno = new JFrame();
+        okno.setTitle("DN12 Tilen Gabršček");
+        okno.setSize(1280, 720);
+        okno.setResizable(false);
+        okno.setLocationRelativeTo(null);
+        okno.setLayout(new BorderLayout());
+        JPanel center = new JPanel();
+        JPanel left = new JPanel();
+        JPanel right = new JPanel();
+
+        JButton pretvori = new JButton("Pretvori -->");
+        JTextArea orinal = new JTextArea(45, 45);
+        JTextArea pretvorba = new JTextArea(45, 45);
+        left.add(orinal);
+        right.add(pretvorba);
+        center.setLayout(new GridBagLayout());
+
+        okno.add(center, BorderLayout.CENTER);
+        okno.add(right, BorderLayout.LINE_END);
+        okno.add(left, BorderLayout.LINE_START);
+        center.add(pretvori);
+
+        pretvori.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pretvorba.setText(orinal.getText().toUpperCase());
+            }
+
+        });
+
+        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okno.setVisible(true);
+
+    }
+}
--- Domace_Naloge/Test.java
+++ Domace_Naloge/Test.java
@@ -2,11 +2,40 @@ import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
-        TreeSet<Integer> neki = new TreeSet<>();
-        neki.add(1);
-        for (Integer integer : neki) {
-            neki.add(++integer);
+        vBin("1025");
+    }
+
+    public static void vBin(String stev) {
+        String orig = stev;
+        String bin = "";
+        while (!(stev.equals("1") || stev.equals("0"))) {
+            switch (stev.charAt(stev.length() - 1)) {
+                case '1':
+                case '3':
+                case '5':
+                case '7':
+                case '9':
+                    bin = "1" + bin;
+                    break;
+                default:
+                    bin = "0" + bin;
+                    break;
+            }
+            stev = polovica(stev);
         }
-        System.out.println(neki);
+        bin = stev + bin;
+        int start = 0;
+        int count = 0;
+        while (bin.indexOf("1", start) != -1) {
+            start = bin.indexOf("1", start) + 1;
+            count++;
+        }
+        System.out.printf("%s(10) = %s(2) -> stevilo bitov: %d\n", orig, bin, count);
+    }
+
+    // Ta metoda ni dew programe je ze napisana (ignore this)
+    public static String polovica(String stev) {
+        int neki = Integer.parseInt(stev);
+        return neki / 2 + "";
     }
 }
